38956aa145684b9ef25fab2084b06ad53ad9f1df
Luis Adorno <luis.adorno5@upr.edu>
2022-06-22 10:30:53 -0400

    Completed HellaLoops01

--- src/main/java/HellaLoops01.java
+++ src/main/java/HellaLoops01.java
@@ -1,33 +1,63 @@
 public class HellaLoops01 {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        StringBuilder builder = new StringBuilder();
+        for(int i = start; i<stop; i++){
+            if(i % 2 == 0)
+                builder.append(i);
+        }
+        return builder.toString();
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        StringBuilder builder = new StringBuilder();
+        for(int i = start; i<stop; i++){
+            if(i % 2 != 0)
+                builder.append(i);
+        }
+        return builder.toString();
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        StringBuilder builder = new StringBuilder();
+        for(int i = start; i<stop; i = i+step){
+            builder.append(i*i);
+        }
+        return builder.toString();
     }
 
-    public static String getRange(int start) {
-        return null;
+    public static String getRange(int stop) {
+        StringBuilder builder = new StringBuilder();
+        for(int i = 0; i<stop; i++){
+            builder.append(i);
+        }
+        return builder.toString();
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        StringBuilder builder = new StringBuilder();
+        for(int i = start; i<stop; i++){
+            builder.append(i);
+        }
+        return builder.toString();
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        StringBuilder builder = new StringBuilder();
+        for(int i = start; i<stop; i += step){
+            builder.append(i);
+        }
+        return builder.toString();
     }
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        StringBuilder builder = new StringBuilder();
+        for(int i = start; i<stop; i+=step){
+            builder.append(((int) Math.pow(i, exponent)));
+        }
+        return builder.toString();
     }
 }
